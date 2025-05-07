23659b5d263879c3a20b5b0cccad7b6d08e5c206
cfinlay <cfinlay@ed.ac.uk>
2025-05-07 11:18:45 +0100

    added hueing

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,15 +9,20 @@ config.font = wezterm.font {
 }
 config.font_size = 14.0
 config.line_height = 1.0
-
-
 config.window_padding = { left = '0.5cell', right = '0.5cell', top = '0.5cell', bottom = '0.5cell' }
 
---config.defaultcursorstyle = 'BlinkingBar'
+config.default_cursor_style = 'BlinkingBar'
 
 config.window_decorations = 'TITLE|RESIZE'
 config.window_background_opacity = 0.96
 config.macos_window_background_blur = 20
+
+config.window_background_opacity = 0.8
+
+config.inactive_pane_hsb = {
+  saturation = 0.1,
+  brightness = 0.4,
+}
 
 config.keys = {
   { key = 'd', mods = 'CMD|SHIFT', action = action.SplitVertical { domain = 'CurrentPaneDomain' } },
@@ -29,6 +34,10 @@ config.keys = {
   { key = 'LeftArrow', mods = 'CMD', action = action.ActivatePaneDirection 'Left' },
   { key = 'RightArrow', mods = 'CMD', action = action.ActivatePaneDirection 'Right' },
   { key = 'LeftArrow', mods = 'CMD|OPT', action = action.ActivateTabRelative(-1) },
-  { key = 'RightArrow', mods = 'CMD|OPT', action = action.ActivateTabRelative(1) }
+  { key = 'RightArrow', mods = 'CMD|OPT', action = action.ActivateTabRelative(1) },
+  { key = 'LeftArrow',  mods = 'CMD|CTRL', action = action.AdjustPaneSize { 'Left',  5 } },
+  { key = 'RightArrow', mods = 'CMD|CTRL', action = action.AdjustPaneSize { 'Right', 5 } },
+  { key = 'UpArrow',    mods = 'CMD|CTRL', action = action.AdjustPaneSize { 'Up',    5 } },
+  { key = 'DownArrow',  mods = 'CMD|CTRL', action = action.AdjustPaneSize { 'Down',  5 } },
 }
 return config
